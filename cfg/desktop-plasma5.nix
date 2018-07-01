376e1089241e7e7f73fd835fd421818dda2159c0
Daniël de Kok <me@danieldk.eu>
2018-07-01 13:50:32 +0200

    Add Plasma 5 configuration.

---
+++ cfg/desktop-plasma5.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      plasma5.enable = true;
+    };
+    displayManager = {
+      sddm.enable = true;
+      sddm.extraConfig = ''
+        [Wayland]
+        SessionCommand=${lib.getBin pkgs.sddm}/share/sddm/scripts/wayland-session
+        SessionDir=/etc/wayland
+      '';
+    };
+  };
+
+  
+  environment.etc."wayland/plasma-wayland.desktop".text = ''
+    [Desktop Entry]
+    Encoding=UTF-8
+    Type=Application
+    Exec=${lib.getBin pkgs.plasma-workspace}/bin/startplasmacompositor
+    TryExec=${lib.getBin pkgs.plasma-workspace}/bin/startplasmacompositor
+    DesktopNames=KDE
+    Name=Plasma-wayland
+  '';
+}
