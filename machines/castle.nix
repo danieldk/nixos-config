39a9628e5aec5f9304b1ad37602ecfb74b3367a8
Daniël de Kok <me@danieldk.eu>
2018-07-15 11:06:28 +0200

    Shuffle around castle boot loader options.

--- machines/castle.nix
+++ machines/castle.nix
@@ -1,15 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  
-  boot.loader.grub.device = "/dev/vda";
-
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
-
   environment.systemPackages = with pkgs; [
     vim
   ];
--- ops/castle/castle-none.nix
+++ ops/castle/castle-none.nix
@@ -3,6 +3,13 @@
     deployment.targetEnv = "none";
     deployment.targetHost = "95.179.147.165";
 
+    boot.loader.grub.enable = true;
+    boot.loader.grub.version = 2;
+    boot.loader.grub.device = "/dev/vda";
+    boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
+    boot.kernelModules = [ ];
+    boot.extraModulePackages = [ ];
+
     fileSystems."/" =
     { device = "/dev/disk/by-uuid/333a5573-81ff-4c8b-b40c-0edf5e5a3b41";
       fsType = "ext4";
