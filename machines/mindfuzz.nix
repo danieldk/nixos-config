f4f65debb49b6ed7e51e6148b4c878187bb2cffc
Daniël de Kok <me@danieldk.eu>
2018-10-15 16:37:27 +0200

    Add initial version of mindfuzz configuration.

---
+++ machines/mindfuzz.nix
@@ -0,0 +1,118 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true; 
+  boot.supportedFilesystems = [ "zfs" ];
+
+  networking.hostName = "mindfuzz";
+  #networking.networkmanager.enable = true;
+
+  i18n = {
+    # Bigger console font for 4k screen.
+    consoleFont = "latarcyrheb-sun32";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+  ];
+
+  nix.buildCores = 2;
+  nix.useSandbox = true;
+
+  programs.bash.enableCompletion = true;
+  programs.vim.defaultEditor = true;
+  programs.zsh.enable = true;
+  programs.zsh.enableCompletion = true;
+
+  # Services
+  services.avahi.enable = true;
+
+  #services.fwupd.enable = true;
+
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    permitRootLogin = "no";
+  };
+
+  #services.plex = {
+  #  enable = true;
+  #  openFirewall = true;
+  #};
+  
+  services.resilio = {
+    enable = true;
+    enableWebUI = true;
+    deviceName = "mindfuzz";
+  };
+
+  services.samba = {
+    enable = true;
+    shares = {
+      homes = {
+        comment = "Home Directories";
+        browseable = "no";
+        writable = "yes";
+      };
+      media = {
+        path = "/srv/media";
+        "valid users" = "daniel mediaclient";
+        browsable = "yes";
+        writable = "no";
+        "write list" = "daniel";
+        "inherit acls" = "yes";
+      };
+    };
+  };
+  services.zfs.autoScrub.enable = true;
+
+  networking.firewall = {
+    enable = true;
+    checkReversePath = false;
+    allowedTCPPorts = [ 22 139 445 9000 ];
+    allowedUDPPorts = [ 137 138 ];
+  };
+
+  networking.hostId = "8bfc957f";
+
+  # Hardware
+  hardware.cpu.intel.updateMicrocode = true;
+
+  users.extraUsers.mediaclient = {
+  };
+
+  users.extraUsers.daniel = {
+    createHome = true;
+    home = "/home/daniel";
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" ];
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICjEndjSNA5r4F5fdM9ZL9v1xY5+vGXYGHBUAQGAt5h3"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAxQ5dl7Md+wbS5IzCjTV4MN3fyo+/aeVJFA6ITCq43lWMMmFluooGi078S8huWFZwjuphJota5g/M3Q/U3G7KiCfDZN4HwucPGT8NQFHntRKQ9DdjJfeD+zE3ZTdKYsXe3N5wI5KSIgZIWk6WA4viZLtVVFHrttDirC30g4H9Cx/OdoIzANDtWAOxkYNeTz/lFnawuzbUasVJsCxYJ7AI6BKhaYqR6Fr12ceHEtmXG5nsZ/r6rHqdZHCknvSx1lSbp/cLReWFvlxtipmbvFHAbaVoc1TsRwExvOw26eSOgjqNFKumriVeOTpIlaZXpzGy+tEHeymmN63fF1UmsHUHBw=="
+    ];
+  };
+
+  system.stateVersion = "18.09";
+}
