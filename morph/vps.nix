ff0ac086ad434966f23fff4cc45889c587a761d6
Daniël de Kok <me@danieldk.eu>
2019-11-27 08:34:59 +0100

    Health check: blob.danieldk.eu reachable over https

--- morph/vps.nix
+++ morph/vps.nix
@@ -2,11 +2,24 @@ let
   pkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.03.tar.gz";
   }) {};
+  addDeployment = machineConfig: deploy: args@{ config, lib, pkgs, ... }:
+    machineConfig args // { deployment = deploy; };
 in {
   network = {
     pkgs = pkgs // { overlays = []; };
     description = "Personal VPSes";
   };
 
-  "syncnode.dekok.dk" = import ../machines/syncnode.nix;
+  "syncnode.dekok.dk" = addDeployment (import ../machines/syncnode.nix) {
+    healthChecks = {
+      http = [
+        {
+          scheme = "https";
+          port = 443;
+          host = "blob.danieldk.eu";
+          description = "Check whether blob.danieldk.eu is reachable.";
+        }
+      ];
+    };
+  };
 }
