7d54424836baf7faf20c8343aecb06dd932e8475
Daniël de Kok <me@danieldk.eu>
2019-11-30 10:58:36 +0100

    Merge hardware configuration

--- machines/castle.nix
+++ machines/castle.nix
@@ -4,9 +4,25 @@ let
   secrets = import ../secrets.nix;
   unstable = import <nixos-unstable> {};
 in {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+  imports =
+    [ <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
+    ];
+
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.kernelModules = [ ];
+  boot.loader.grub.device = "/dev/vda";
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/333a5573-81ff-4c8b-b40c-0edf5e5a3b41";
+      fsType = "ext4";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/45612f70-4b3b-4b4c-bdb7-c6ac962ed6b0"; }
+    ];
+
+  nix.maxJobs = 1;
 
   #boot.kernelPackages = pkgs.linuxPackages_hardened;
   boot.kernelPackages = pkgs.linuxPackages;
