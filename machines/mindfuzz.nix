34993c99d5d39381c566d781c1bdf7c435c9b0ca
Daniël de Kok <me@danieldk.eu>
2019-11-30 11:03:57 +0100

    mindfuzz: permit root logins

--- machines/mindfuzz.nix
+++ machines/mindfuzz.nix
@@ -98,7 +98,6 @@
   services.openssh = {
     enable = true;
     passwordAuthentication = false;
-    permitRootLogin = "no";
   };
 
   #services.plex = {
@@ -145,7 +144,14 @@
   hardware.cpu.intel.updateMicrocode = true;
   hardware.enableAllFirmware = true;
 
-  users.extraUsers = {
+  users.users = {
+    root = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICjEndjSNA5r4F5fdM9ZL9v1xY5+vGXYGHBUAQGAt5h3"
+        "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAxQ5dl7Md+wbS5IzCjTV4MN3fyo+/aeVJFA6ITCq43lWMMmFluooGi078S8huWFZwjuphJota5g/M3Q/U3G7KiCfDZN4HwucPGT8NQFHntRKQ9DdjJfeD+zE3ZTdKYsXe3N5wI5KSIgZIWk6WA4viZLtVVFHrttDirC30g4H9Cx/OdoIzANDtWAOxkYNeTz/lFnawuzbUasVJsCxYJ7AI6BKhaYqR6Fr12ceHEtmXG5nsZ/r6rHqdZHCknvSx1lSbp/cLReWFvlxtipmbvFHAbaVoc1TsRwExvOw26eSOgjqNFKumriVeOTpIlaZXpzGy+tEHeymmN63fF1UmsHUHBw=="
+      ];
+    };
+
     daniel = {
       createHome = true;
       home = "/home/daniel";
