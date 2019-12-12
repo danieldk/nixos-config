d2d1a2cf70d4e3f82054a02058a91a0e45525564
Daniël de Kok <me@danieldk.eu>
2019-12-12 20:49:33 +0100

    Use one shared list of SSH public keys

--- machines/castle.nix
+++ machines/castle.nix
@@ -59,10 +59,7 @@ in {
       home = "/home/daniel";
       extraGroups = [ "wheel" ];
       isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICjEndjSNA5r4F5fdM9ZL9v1xY5+vGXYGHBUAQGAt5h3"
-        "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAxQ5dl7Md+wbS5IzCjTV4MN3fyo+/aeVJFA6ITCq43lWMMmFluooGi078S8huWFZwjuphJota5g/M3Q/U3G7KiCfDZN4HwucPGT8NQFHntRKQ9DdjJfeD+zE3ZTdKYsXe3N5wI5KSIgZIWk6WA4viZLtVVFHrttDirC30g4H9Cx/OdoIzANDtWAOxkYNeTz/lFnawuzbUasVJsCxYJ7AI6BKhaYqR6Fr12ceHEtmXG5nsZ/r6rHqdZHCknvSx1lSbp/cLReWFvlxtipmbvFHAbaVoc1TsRwExvOw26eSOgjqNFKumriVeOTpIlaZXpzGy+tEHeymmN63fF1UmsHUHBw=="
-      ];
+      openssh.authorizedKeys.keys = import ../cfg/danieldk-keys.nix;
     };
 
     doerte = {
