e230b636d6ee3be0259a7d486fb365ce51399d5f
Daniël de Kok <me@danieldk.eu>
2019-12-22 09:43:33 +0100

    trex: use autologin
    No password loging does not seem to work anymore?

--- machines/trex.nix
+++ machines/trex.nix
@@ -115,6 +115,11 @@
     nopasswdlogin.gid = null;
   };
 
+  services.xserver.displayManager.gdm.autoLogin = {
+    enable = true;
+    user = "liset";
+  };
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
