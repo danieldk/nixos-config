89fec57b30e36d0dc2eb7ca47b0353a5903c3854
Daniël de Kok <me@danieldk.eu>
2020-05-10 14:12:14 +0200

    morph: use 20.03 on desktops

--- morph/desktops.nix
+++ morph/desktops.nix
@@ -1,6 +1,6 @@
 let
   pkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.09.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/nixos-20.03.tar.gz";
   }) {};
   addDeployment = machineConfig: deploy: args@{ config, lib, pkgs, ... }:
     machineConfig args // { deployment = deploy; };
